7844470414d63d5cdbf3b21a938e53cc5d24c5a6
chosangho <teha007@naver.com>
2021-11-01 19:49:11 +0900

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Navbar from '../components/nav.svelte';
+</script>
+
+<Navbar />
+<div class="p-8 max-w-6xl mx-auto">
+	<slot />
+</div>
+
+<style>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
